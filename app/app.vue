e6751dece5963ba78274d98b164cd47d3dcc5ca1
bdecker-navi <bdecker@navistone.com>
2025-05-15 10:48:19 -0400

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,71 @@
+<script setup>
+useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+
+const title = 'TODO App'
+
+useSeoMeta({
+  title
+})
+</script>
+
+<template>
+  <UApp>
+    <UHeader>
+      <!--      <template #left> -->
+      <!--        <NuxtLink to="/"> -->
+      <!--          <LogoPro class="w-auto h-6 shrink-0" /> -->
+      <!--        </NuxtLink> -->
+
+      <!--        <TemplateMenu /> -->
+      <!--      </template> -->
+
+      <template #right>
+        <UColorModeButton />
+
+        <!--        <UButton -->
+        <!--          to="https://github.com/nuxt-ui-pro/starter" -->
+        <!--          target="_blank" -->
+        <!--          icon="i-simple-icons-github" -->
+        <!--          aria-label="GitHub" -->
+        <!--          color="neutral" -->
+        <!--          variant="ghost" -->
+        <!--        /> -->
+      </template>
+    </UHeader>
+
+    <UMain>
+      <NuxtPage />
+    </UMain>
+
+    <USeparator icon="i-simple-icons-nuxtdotjs" />
+
+    <UFooter>
+      <!--      <template #left> -->
+      <!--        <p class="text-sm text-muted"> -->
+      <!--          Copyright © {{ new Date().getFullYear() }} -->
+      <!--        </p> -->
+      <!--      </template> -->
+
+      <!--      <template #right> -->
+      <!--        <UButton -->
+      <!--          to="https://github.com/nuxt-ui-pro/starter" -->
+      <!--          target="_blank" -->
+      <!--          icon="i-simple-icons-github" -->
+      <!--          aria-label="GitHub" -->
+      <!--          color="neutral" -->
+      <!--          variant="ghost" -->
+      <!--        /> -->
+      <!--      </template> -->
+    </UFooter>
+  </UApp>
+</template>
---
+++ app/pages/to-do.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const todos = ref([{
+  id: 1,
+  title: 'Buy groceries',
+  completed: false
+},
+{
+  id: 2,
+  title: 'Clean the house',
+  completed: true
+}])
+</script>
+
+<template>
+  <UPageList>
+    <UPageCard
+      v-for="todo in todos"
+      :key="todo.id"
+    >
+      <template #body>
+        {{ todo.title }}
+      </template>
+    </UPageCard>
+  </UPageList>
+</template>
+
+<style scoped>
+
+</style>
